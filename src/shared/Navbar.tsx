396394b9652ac15c8b6e3de5781d8265f3b0b657
poranmia2222 <poranmia2222@gmail.com>
2026-09-19 23:00:57 +0600

    Navbar complete

---
+++ src/shared/Navbar.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const Navbar = () => {
+    const links = <>
+        <li><a href="">Home</a></li>
+        <li><a>Listed Books</a></li>
+        <li><a>Pages to Read</a></li>
+    </>
+    return (
+        <nav className='bg-base-100 shadow-sm'>
+            <div className="navbar container mx-auto">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg aria-label="Menu" xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                        </div>
+                        <ul
+                            tabIndex={-1}
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2">
+                            {links}
+                        </ul>
+                    </div>
+                    <a className="btn btn-ghost text-xl font-bold">Books Vibe</a>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1 text-lg">
+                        {links}
+                    </ul>
+                </div>
+                <div className="navbar-end gap-3">
+                    <a className="btn bg-[#23BE0A] text-white">Sign In</a>
+                    <a className="btn bg-[#59C6D2] text-white">Sign Up</a>
+                </div>
+            </div>
+        </nav >
+    );
+};
+
+export default Navbar;
